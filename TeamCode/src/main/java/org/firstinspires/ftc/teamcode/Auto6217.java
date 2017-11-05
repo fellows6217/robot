d5f6bde62bdd203f63e84a994d0fdef54375d0ba
josie <Jelly1667@gmail.com>
2017-11-05 15:19:14 -0500

    Changed turn speed to 100

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto6217.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto6217.java
@@ -102,7 +102,7 @@ public class Auto6217 extends LinearOpMode {
     // These constants define the desired driving/control characteristics
     // The can/should be tweaked to suite the specific robot drive train.
     static final double     DRIVE_SPEED             = 0.7;     // Nominal speed for better accuracy.
-    static final double     TURN_SPEED              = 0.5;     // Nominal half speed for better accuracy.
+    static final double     TURN_SPEED              = 1;     // 100% power, full speed
 
     static final double     HEADING_THRESHOLD       = 1 ;      // As tight as we can make it with an integer gyro
     static final double     P_TURN_COEFF            = 0.1;     // Larger is more responsive, but also less stable
